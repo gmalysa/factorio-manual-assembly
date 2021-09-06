ce99f6c6a70031e8de0a4b28e46baa9897eab586
Greg Malysa <gmalysa@alumni.stanford.edu>
2021-09-06 02:47:20 -0400

    Add sequence recipes which require a inputs to change according to a pattern to operate the assembler

--- controls/assemblers.lua
+++ controls/assemblers.lua
@@ -37,8 +37,8 @@ local function generate_null_recipe()
 end
 
 ---
- -- Enable recipes are the tier 1 of assembler control: drive them with a specific signal
- -- to enable the assembler, usually the single ingredient used
+ -- Enable recipes are the tier 1 of assembler control: drive them with a specific
+ -- signal to enable the assembler, usually the single ingredient used
  --
 local function generate_enable_recipe(signal)
 	-- Initialize the state with the required signal array
@@ -51,6 +51,35 @@ local function generate_enable_recipe(signal)
 
 	-- Next state is same as previous state, these are not dynamic
 	local step = function(state)
+	end
+
+	return build_recipe({
+		init = init,
+		step = step
+	})
+end
+
+---
+ -- Sequence recipes are like tier 2, drive them with a specific order of changing
+ -- signals to enable the assembler
+ -- @param[in] signals Array of signal requirements, where each signal requirement
+ --                    is an array of {signal, value}. So,
+ --                    { { {signal, value}, {signal, value} }, { ... }, { ... } }
+ --                    for three states, where the first has two required signals
+ --
+local function generate_sequence_recipe(signals)
+	local init = function(state)
+		state.index = 1
+		state.signals = signals[1]
+		state.count = table_size(signals)
+	end
+
+	local step = function(state)
+		state.index = state.index + 1
+		if state.index > state.count then
+			state.index = 1
+		end
+		state.signals = signals[state.index]
 	end
 
 	return build_recipe({
@@ -73,5 +102,16 @@ this["null-recipe"] = generate_null_recipe()
 this["iron-gear-wheel"] = generate_enable_recipe(iron_plate)
 this["iron-stick"] = generate_enable_recipe(iron_plate)
 this["copper-cable"] = generate_enable_recipe(copper_plate)
+
+this["inserter"] = generate_sequence_recipe({
+	{{
+		signal = iron_plate,
+		value = 1
+	}},
+	{{
+		signal = copper_plate,
+		value = 1
+	}}
+})
 
 return this
